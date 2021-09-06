3c64e0d40c7142f2977a5218e37586a6e81cbbf2
anish <anis@somethignh.com>
2021-09-06 09:22:36 +0530

    bux fixed

--- test/app.js
+++ test/app.js
@@ -5,6 +5,8 @@ const app = express();
 app.get( '/' ,(req ,res)=> {
 
     res.send('<h1>This is a heading </h1>')
+    
+})
 
 app.listen(process.env.PORT , ()=> {
     console.log("server is running on port 3000")
