5f0e38e26a00166becd0294763230f074e63da68
anish <anis@somethignh.com>
2021-09-06 09:20:40 +0530

    changed port to default

--- test/app.js
+++ test/app.js
@@ -5,9 +5,7 @@ const app = express();
 app.get( '/' ,(req ,res)=> {
 
     res.send('<h1>This is a heading </h1>')
-    
-})
 
-app.listen(3000 , ()=> {
+app.listen(process.env.PORT , ()=> {
     console.log("server is running on port 3000")
 })
